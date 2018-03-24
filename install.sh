38bc6acc89f1e05771e524d01cb5ac55cd9a2536
Minlin Zhang <zhangminlin.pku@gmail.com>
2018-03-23 18:46:53 -0700

    print dependencies in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# color code: https://stackoverflow.com/a/5947802
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;34m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
+
 VIMRC="${HOME}/.vimrc"
 VIMRC_LOCAL="${HOME}/.vimrc.local"
 
@@ -13,16 +21,21 @@ if [ -f $VIMRC_LOCAL ]; then
 fi
 
 # update vimrc settings
-echo -e "\nupdate vimrc settings..."
+echo -e "\nINFO: update vimrc settings..."
 cp "vimrc" $VIMRC
 cp "vimrc.local" $VIMRC_LOCAL
 
 # install the bundles
-echo -e "\ninstall vim bundles..."
+echo -e "\nINFO: install vim bundles..."
 vim +NeoBundleInstall +qall
 
 # install patched font
-echo -e "\nPlease install 'Monaco for Powerline' font manually if you haven't!"
-echo -e "url: https://gist.github.com/Miliion-Zhang/3510f068a80d2735fd87b805c1052edb"
+echo -e "\n${RED}WARN:${NC} Please install ${PURPLE}'Monaco for Powerline'${NC} font manually if you haven't!"
+echo -e "      url: https://gist.github.com/Miliion-Zhang/3510f068a80d2735fd87b805c1052edb"
 
-echo -e "\n\e[32mall done! enjoy it!\e[39m"
+# let user know the dependencies
+echo -e "\n${RED}WARN:${NC} VIM should be built with ${PURPLE}+conceal${NC}(for indentLine) ${PURPLE}+lua${NC}(for neocomplete) !!!"
+echo -e "      You can install VIM using brew on Mac or linuxbrew on Ubuntu."
+echo -e "\n${RED}WARN:${NC} Besides, ${PURPLE}'ctags'${NC}(for tagbar) and '${PURPLE}ack'${NC} are also required."
+
+echo -e "\nINFO: ${GREEN}all done! enjoy it!${NC}"
